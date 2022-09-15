8de230339fa2657474c945ecf7ec1d4d492f2272
nesa402610 <argyleferris@gmail.com>
2022-09-15 14:24:21 +0300

    Курсор поинтер

--- resources/js/components/UI/certCard.tsx
+++ resources/js/components/UI/certCard.tsx
@@ -12,7 +12,7 @@ const CertCard: FC<certCardProps> = ({handler}) => {
     return (
         <>
             {certificates.map(cert =>
-                <div key={cert.name} className={'flex rounded-lg overflow-hidden'}>
+                <div key={cert.name} className={'flex rounded-lg overflow-hidden cursor-pointer'}>
                     <img onClick={e => previewImage(e, cert.img)}
                          src={cert.img}
                          alt={cert.name}/>
