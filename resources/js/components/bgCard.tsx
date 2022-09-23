15cd454a2c212a88b010e653dfd3e9366b33679e
nesa402610 <argyleferris@gmail.com>
2022-09-23 22:24:16 +0300

    Добавлять классы можно

--- resources/js/components/bgCard.tsx
+++ resources/js/components/bgCard.tsx
@@ -2,11 +2,12 @@ import React, {FC} from 'react';
 
 interface bgCardProps {
     children: React.ReactNode
+    className?: string
 }
 
-const BgCard: FC<bgCardProps> = ({children}) => {
+const BgCard: FC<bgCardProps> = ({children, className}) => {
     return (
-        <div className={'flex gap-4 xs:flex-col sm:flex-row justify-between bg-stone-600 p-4 rounded-lg'}>
+        <div className={'flex gap-4 xs:flex-col sm:flex-row justify-between bg-stone-600 p-4 rounded-lg ' + className}>
             {children}
         </div>
     );
--- resources/js/components/fgCard.tsx
+++ resources/js/components/fgCard.tsx
@@ -2,10 +2,11 @@ import React, {FC} from 'react';
 
 interface fgCardProps {
     children: React.ReactNode
+    className?: string
 }
-const FgCard: FC<fgCardProps> = ({children}) => {
+const FgCard: FC<fgCardProps> = ({children, className}) => {
     return (
-        <div className={'flex-1 bg-stone-700 shadow-md rounded-lg p-4'}>
+        <div className={'flex-1 bg-stone-700 shadow-md rounded-lg p-4 ' + className}>
             {children}
         </div>
     );
