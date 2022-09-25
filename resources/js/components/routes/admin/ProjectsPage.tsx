cd86922a86874fc143653a43c5d02c4f123f0308
nesa402610 <argyleferris@gmail.com>
2022-09-26 02:31:48 +0300

    Ренейм пропсов

--- resources/js/components/routes/admin/ProjectsPage.tsx
+++ resources/js/components/routes/admin/ProjectsPage.tsx
@@ -7,8 +7,8 @@ const ProjectsPage = () => {
     return (
         <div className={'p-4'}>
             <div className={'grid lg:grid-cols-4 md:grid-cols-2 2xl:grid-cols-5 sm:grid-cols-1 gap-4 px-4'}>
-                {projects.map(site =>
-                    <ProjectCard key={site.name} site={site}/>
+                {projects.map(project =>
+                    <ProjectCard key={project.name} project={project}/>
                 )}
             </div>
         </div>
