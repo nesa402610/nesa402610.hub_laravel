18ec65117dfcd38e47133ae6315a89d1a5bbbff1
nesa402610 <argyleferris@gmail.com>
2022-09-26 02:44:29 +0300

    Favicons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="yandex-verification" content="5d64da24dba353a3"/>
     <meta name="keywords" content="nesa, n/esa, nesa402610, Ferris Argyle">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#44403c">
     <title>nesa</title>
     @viteReactRefresh
     @vite('resources/js/index.tsx')
