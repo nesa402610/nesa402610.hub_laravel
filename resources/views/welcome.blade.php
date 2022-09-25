2d4743b609bb21747fa1a0b7b90158d79fd11264
nesa402610 <argyleferris@gmail.com>
2022-09-26 02:31:33 +0300

    Ключевые слова

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="yandex-verification" content="5d64da24dba353a3"/>
+    <meta name="keywords" content="nesa, n/esa, nesa402610, Ferris Argyle">
     <title>nesa</title>
     @viteReactRefresh
     @vite('resources/js/index.tsx')
