750b61b33e8256e040a1e21d5141a7b01c7b4c3a
nesa402610 <argyleferris@gmail.com>
2022-09-26 00:31:12 +0300

    Верификация для яндекса

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,15 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>nesa</title>
-@viteReactRefresh
-@vite('resources/js/index.tsx')
 <head>
-    <body>
-        <div id="senpai"></div>
-    </body>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="yandex-verification" content="5d64da24dba353a3"/>
+    <title>nesa</title>
+    @viteReactRefresh
+    @vite('resources/js/index.tsx')
+</head>
+<body>
+<div id="senpai"></div>
+</body>
 </html>
