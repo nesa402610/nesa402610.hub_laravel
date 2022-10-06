2668ef4d9f4e39f041c70608aa73f43efe354baf
nesa402610 <argyleferris@gmail.com>
2022-10-06 20:42:58 +0300

    Небольшая валидация

--- resources/js/components/routes/loginPage.tsx
+++ resources/js/components/routes/loginPage.tsx
@@ -18,12 +18,16 @@ interface userProps {
 
 const RegistrationPage: FC = () => {
     const [user, setUser] = useState<userProps>({password: '', email: ''})
-    const [errors, setErrors] = useState<boolean>(false)
+    const [errors, setErrors] = useState(null)
     const navigate = useNavigate()
     const dispatch = useDispatch()
 
     const registrationHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
+            console.log(user.email, user.password, errors)
+        if (!user.email || !user.password) {
+            return setErrors({email: user.email, password: user.password});
+        }
         axios.post('/login',
             {
                 password: user.password,
@@ -40,33 +44,35 @@ const RegistrationPage: FC = () => {
     }
     return (
         <div className={'flex flex-col items-center justify-center'}>
-            <div className={'flex flex-col gap-4 bg-stone-600 p-4 rounded-lg'}>
+            <form className={'flex flex-col gap-4 bg-stone-600 p-4 rounded-lg'}>
                 <h3 className={'text-center text-2xl'}>Вход в аккаунт</h3>
                 {errors ? <h4 className={'text-center text-rose-400 text-lg'}>Проверьте данные. с ними все
                                                                               впорядке?</h4> : ''}
                 <div>
                     <label>Email</label>
-                    {/*{errors.name ? <span>Проверьте правильность Email</span> : ''}*/}
+                    {errors?.email === '' ? <span>Проверьте правильность Email</span> : ''}
                     <Input onChange={e => setUser({...user, email: e.target.value})}
                            type={'email'}
                            bg={'bg-stone-700'}
+                           required={true}
                            placeholder={'yamero0923@nesa.xyz'}/>
                 </div>
                 <div>
                     <label>Пароль</label>
-                    {/*{errors.name ? <span>Минимальная длинна 8 символов</span> : ''}*/}
+                    {errors?.password === '' ? <span>Поле должно быть заполнено</span> : ''}
                     <Input onChange={e => setUser({...user, password: e.target.value})}
                            type={'password'}
                            bg={'bg-stone-700'}
+                           required={true}
                            placeholder={'QWERTY00'}/>
                 </div>
-                <div>
+
                     <button className={'w-full bg-stone-500 p-2 rounded-lg'}
                             onClick={e => registrationHandler(e)}>
                         Войти
                     </button>
-                </div>
-            </div>
+
+            </form>
         </div>
     );
 };
