72bc9a6b7ec3a45de049d7108d6800e14cb16de6
nesa402610 <argyleferris@gmail.com>
2022-10-17 14:45:04 +0300

    Removed unused states and props

--- resources/js/components/profilePage/securityTab.tsx
+++ resources/js/components/profilePage/securityTab.tsx
@@ -1,18 +1,12 @@
-import React, {useState} from 'react';
+import React from 'react';
 import SecurityTabPassword from "./securityTab__password";
 import SecurityTabEmail from "./securityTab__email";
 
 const SecurityTab = () => {
-    const [passwords, setPasswords] = useState(
-        {current: '', newPass: ''}
-    )
-    const [emails, setEmails] = useState({
-        current: '', newEmail: '',
-    })
     return (
         <>
-            <SecurityTabPassword passwords={passwords} setPasswords={setPasswords}/>
-            <SecurityTabEmail emails={{setEmails, emails}} passwords={{passwords, setPasswords}}/>
+            <SecurityTabPassword/>
+            <SecurityTabEmail/>
         </>
     );
 };
