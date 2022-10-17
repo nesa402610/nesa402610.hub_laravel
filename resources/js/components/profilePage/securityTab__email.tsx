5bd852e69cfcb1345276cd90fc3fe5931ab099c6
nesa402610 <argyleferris@gmail.com>
2022-10-17 14:44:17 +0300

    Submit update

--- resources/js/components/profilePage/securityTab__email.tsx
+++ resources/js/components/profilePage/securityTab__email.tsx
@@ -1,34 +1,64 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Input from "../UI/input";
 import FgCard from "../fgCard";
+import SubmitButton from "../UI/submitButton";
+import axios from "axios";
 
-const SecurityTabEmail = ({emails, passwords}) => {
+interface dataProps {
+    currentEmail: string
+    newEmail: string
+    currentPassword: string
+}
+
+const SecurityTabEmail = () => {
+    const [data, setData] = useState<dataProps>(
+        {
+            currentEmail: '',
+            newEmail: '',
+            currentPassword: ''
+        }
+    )
+    const [errors, setErrors] = useState(null)
+
+    const updateEmail = (e) => {
+        e.preventDefault()
+        axios.post('/edit/updateEmail', data)
+            .then(r => console.log(r.data))
+            .catch(err => setErrors(err.response.data.error))
+    }
+
     return (
-        <FgCard className={'flex flex-col gap-2'}>
+        <FgCard>
             <h2 className={'text-2xl text-center'}>Смена почты</h2>
-            <div>
-                <label htmlFor="">Текущий пароль</label>
-                <Input type={'password'}
-                       required={true}
-                       value={passwords.passwords.current}
-                       onChange={e => passwords.setPasswords({...passwords, current: e.target.value})}
-                       placeholder={'Текущий пароль'}/>
-            </div>
-            <div>
-                <label htmlFor="">Текущий Email</label>
-                <Input type={'email'}
-                       required={true}
-                       value={emails.emails.current}
-                       onChange={e => emails.setEmails({...emails.emails, current: e.target.value})}
-                       placeholder={'Новый пароль'}/>
-            </div>
-            <div>
-                <label htmlFor="">Новый Email</label>
-                <Input type={'email'}
-                       required={true}
-                       value={emails.emails.newEmail}
-                       onChange={e => emails.setEmails({...emails.emails, newEmail: e.target.value})}
-                       placeholder={'Новый пароль'}/>
+            {errors && <h3 className={'text-center text-xl text-red-400'}>{errors}</h3>}
+            <div className={'flex flex-col gap-4'}>
+                <label>
+                    Текущий пароль
+                    <Input type={'password'}
+                           required={true}
+                           value={data.currentPassword}
+                           onChange={e => setData({...data, currentPassword: e.target.value})}
+                           placeholder={'Текущий пароль'}/>
+                </label>
+                <label>
+                    Текущий Email
+                    <Input type={'email'}
+                           required={true}
+                           value={data.currentEmail}
+                           onChange={e => setData({...data, currentEmail: e.target.value})}
+                           placeholder={'Текущий email'}/>
+                </label>
+                <label>
+                    Новый Email
+                    <Input type={'email'}
+                           required={true}
+                           value={data.newEmail}
+                           onChange={e => setData({...data, newEmail: e.target.value})}
+                           placeholder={'Новый email'}/>
+                </label>
+                <SubmitButton onClick={updateEmail}>
+                    Обновить Email
+                </SubmitButton>
             </div>
         </FgCard>
     );
--- resources/js/components/profilePage/securityTab__password.tsx
+++ resources/js/components/profilePage/securityTab__password.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Input from "../UI/input";
 import FgCard from "../fgCard";
+import SubmitButton from "../UI/submitButton";
+import axios from "axios";
 
-const SecurityTabPassword = ({passwords,setPasswords}) => {
+
+const SecurityTabPassword = () => {
+    const [data, setData] = useState(
+        {currentPassword: '', newPassword: ''}
+    )
+    const [errors, setErrors] = useState(null)
+
+    const updatePassword = (e) => {
+        e.preventDefault()
+        setErrors(null)
+        axios.post('/edit/updatePassword', data).then(r => {
+            console.log(r.data)
+        }).catch(err => {
+            setErrors(err.response.data.error)
+        })
+    }
     return (
-        <FgCard className={'flex flex-col gap-2'}>
+        <FgCard>
             <h2 className={'text-center text-2xl'}>Смена пароля</h2>
-            <div>
-                <label htmlFor="">Текущий пароль</label>
-                <Input type={'password'}
-                       required={true}
-                       value={passwords.current}
-                       onChange={e => setPasswords({...passwords, current: e.target.value})}
-                       placeholder={'Текущий пароль'}/>
-            </div>
-            <div>
-                <label htmlFor="">Новый пароль</label>
-                <Input type={'password'}
-                       required={true}
-                       value={passwords.newPass}
-                       onChange={e => setPasswords({...passwords, newPass: e.target.value})}
-                       placeholder={'Новый пароль'}/>
+            {errors && <h3 className={'text-center text-xl text-red-400'}>{errors}</h3>}
+            <div className={'flex flex-col gap-4'}>
+                <label>
+                    Текущий пароль
+                    <Input type={'password'}
+                           required={true}
+                           value={data.currentPassword}
+                           onChange={e => setData({...data, currentPassword: e.target.value})}
+                           placeholder={'Текущий пароль'}/>
+                </label>
+                <label>
+                    Новый пароль
+                    <Input type={'password'}
+                           required={true}
+                           value={data.newPassword}
+                           onChange={e => setData({...data, newPassword: e.target.value})}
+                           placeholder={'Новый пароль'}/>
+                </label>
+                <SubmitButton onClick={updatePassword}>Обновить пароль</SubmitButton>
             </div>
         </FgCard>
     );
