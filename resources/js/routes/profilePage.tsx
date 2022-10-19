665bc41ac6333473c14565615e60ce9ea2049db8
nesa402610 <argyleferris@gmail.com>
2022-10-19 13:21:40 +0300

    Небольшие фиксы и адаптив под мобайл

--- resources/js/routes/profilePage.tsx
+++ resources/js/routes/profilePage.tsx
@@ -5,33 +5,36 @@ import {Link} from "react-router-dom";
 import {IoSettingsSharp} from "react-icons/io5";
 import {IUser} from "../types/types";
 import FgCard from "../components/fgCard";
+import moment from "moment";
 
 const ProfilePage = () => {
     const user = useTypedSelector(state => state.auth.user) as IUser
     if (user) return (
         <div className={'p-4'}>
             <BgCard>
-                <div className={'flex gap-4 w-full'}>
-                    <div className={'flex flex-col gap-2 items-end'}>
+                <div className={'flex sm:flex-row xs:flex-col gap-4 w-full'}>
+                    <div className={'flex xs:items-center flex-col gap-2 items-end'}>
                         <img className={'rounded-lg'}
                              width={'200px'}
                              height={'200px'}
                              src={user.avatar}
                              alt="user profile picture"/>
-                        <Link to={'edit'} className={'hover:text-stone-400 transition-colors flex gap-2 text-2xl items-center'}>
+                        <Link to={'edit'}
+                              className={'hover:text-stone-400 transition-colors flex gap-2 text-2xl items-center'}>
                             <span className={'text-lg'}>Настройки</span>
                             <IoSettingsSharp/>
                         </Link>
                     </div>
                     <div className={'flex flex-col gap-8 flex-1'}>
-                       <div>
-                           <div>
-                               {user.name} {user.lastName} {user.middleName}
-                           </div>
-                           <div>
-                               {user.birthday} {user.status && '/ ' + user.status} / {user.created_at}
-                           </div>
-                       </div>
+                        <div>
+                            <div>
+                                {user.name} {user.lastName} {user.middleName}
+                            </div>
+                            <div>
+                                {moment(user.birthday).format('DD.MM.YYYY')}&nbsp;
+                                {user.status && '/ ' + user.status} / Впервые с {moment(user.created_at).format('YYYY')} года
+                            </div>
+                        </div>
                         <div className={'flex flex-col flex-1'}>
                             <h2 className={'font-bold mb-2 italic text-end mr-2'}>Информация обо мне</h2>
                             <FgCard className={'shadow-md'}>
