b30a54ae6bb31ab336f3e785474f81964796b2e0
nesa402610 <argyleferris@gmail.com>
2022-10-19 16:42:39 +0300

    Костыль для PhpStorm, чтобы видеть ORM подсказки. Здорово они это придумали

--- app/Models/Certificates.php
+++ app/Models/Certificates.php
@@ -5,6 +5,30 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Certificates
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $author
+ * @property string $sign
+ * @property string $date
+ * @property string $image
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Certificates newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Certificates newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Certificates query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Certificates whereAuthor($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Certificates whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Certificates whereDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Certificates whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Certificates whereImage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Certificates whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Certificates whereSign($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Certificates whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Certificates extends Model
 {
     use HasFactory;
--- app/Models/Rating.php
+++ app/Models/Rating.php
@@ -5,6 +5,27 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Rating
+ *
+ * @property int $id
+ * @property int $project_id
+ * @property int|null $user_id
+ * @property int $rating
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Project $project
+ * @method static \Illuminate\Database\Eloquent\Builder|Rating newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Rating newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Rating query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Rating whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Rating whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Rating whereProjectId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Rating whereRating($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Rating whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Rating whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Rating extends Model
 {
     use HasFactory;
