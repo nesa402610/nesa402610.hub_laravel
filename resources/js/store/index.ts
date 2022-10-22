dbdcd357107e78efbb1ceac0a49b59110b60b8a0
nesa402610 <argyleferris@gmail.com>
2022-10-22 16:25:21 +0300

    Redux-thunk

--- resources/js/store/index.ts
+++ resources/js/store/index.ts
@@ -1,13 +1,15 @@
-import {combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, createStore} from "redux";
 import {authReducer} from "./authReducer";
 import {homePageReducer} from "./homePageReducer";
 import {modalReducer} from "./modalReducer";
+import {composeWithDevTools} from "@redux-devtools/extension";
+import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
     auth: authReducer,
     homePage: homePageReducer,
     modal: modalReducer
 })
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 export type RootState = ReturnType<typeof rootReducer>
