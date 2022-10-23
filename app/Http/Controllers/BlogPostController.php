5ac06c4aed8ff4558780c20490c69f3adf486e19
nesa402610 <argyleferris@gmail.com>
2022-10-23 20:38:09 +0300

    Контроллер блога

---
+++ app/Http/Controllers/BlogPostController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BlogPost;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class BlogPostController extends Controller
+{
+    public function getAllPosts() {
+        $posts = BlogPost::orderBy('id', 'desc')->get();
+        return response($posts, 200);
+    }
+
+    public function createPost(Request $request) {
+        $post = new BlogPost;
+
+        $post->user_id = Auth::user()->id;
+        $post->title = $request->title;
+        $post->body = $request->body;
+        $post->save();
+        return response($post);
+    }
+}
