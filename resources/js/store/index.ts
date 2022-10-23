1cf5b093135d28406b757b189e7f8ade6cce2dab
nesa402610 <argyleferris@gmail.com>
2022-10-23 20:37:27 +0300

    Добавлен редусер блога в рутовый редусер

--- resources/js/store/index.ts
+++ resources/js/store/index.ts
@@ -4,11 +4,13 @@ import thunk from "redux-thunk";
 import {authReducer} from "./reducers/authReducer";
 import {homePageReducer} from "./reducers/homePageReducer";
 import {modalReducer} from "./reducers/modalReducer";
+import {BlogReducer} from "./reducers/blogReducer";
 
 const rootReducer = combineReducers({
     auth: authReducer,
     homePage: homePageReducer,
-    modal: modalReducer
+    modal: modalReducer,
+    posts: BlogReducer,
 })
 export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
