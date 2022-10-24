b1c0c1c1c20384f6a143c0491de7159348aa144b
nesa402610 <argyleferris@gmail.com>
2022-10-24 17:03:30 +0300

    Добавлено поле видимости поста

--- database/migrations/2022_10_23_113152_create_blog_posts_table.php
+++ database/migrations/2022_10_23_113152_create_blog_posts_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->string('title');
             $table->string('body');
             $table->string('image')->nullable();
+            $table->tinyInteger('visibility')->default(0);
             $table->timestamps();
         });
     }
