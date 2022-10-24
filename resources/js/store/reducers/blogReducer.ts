bfd132216f36c0a5fc7f517aca34b9d2de6f4b58
nesa402610 <argyleferris@gmail.com>
2022-10-24 17:00:10 +0300

    Добавлены редусеры удаления и обновления постов, как и экшен криейторы

--- resources/js/store/reducers/blogReducer.ts
+++ resources/js/store/reducers/blogReducer.ts
@@ -10,6 +10,8 @@ interface postsState {
 
 const SET_POSTS = 'SET_POSTS'
 const ADD_POST = 'ADD_POST'
+const EDIT_POST = 'EDIT_POST'
+const DELETE_POST = 'DELETE_POST'
 
 export const BlogReducer = (state = defaultState, action: IActionProps): postsState => {
     switch (action.type) {
@@ -17,6 +19,15 @@ export const BlogReducer = (state = defaultState, action: IActionProps): postsSt
             return {...state, posts: action.payload}
         case ADD_POST:
             return {...state, posts: [action.payload, ...state.posts]}
+        case EDIT_POST:
+            return {
+                ...state, posts: state.posts.map(post => post.id === action.payload.id
+                    ?
+                    action.payload
+                    : post)
+            }
+        case DELETE_POST:
+            return {...state, posts: state.posts.filter(post => post.id !== action.payload)}
         default:
             return state
     }
@@ -24,3 +35,5 @@ export const BlogReducer = (state = defaultState, action: IActionProps): postsSt
 
 export const setPostsActions = (payload) => ({type: SET_POSTS, payload})
 export const addPostActions = (payload) => ({type: ADD_POST, payload})
+export const editPostActions = (payload) => ({type: EDIT_POST, payload})
+export const deletePostActions = (payload) => ({type: DELETE_POST, payload})
