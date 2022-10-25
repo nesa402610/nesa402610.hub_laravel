16243c02bb766df1bdeff693e00c83cf070cb2ed
nesa402610 <argyleferris@gmail.com>
2022-10-25 22:03:40 +0300

    Декомпоз и фикс длинны тела поста

--- resources/js/components/blog/PostCard.tsx
+++ resources/js/components/blog/PostCard.tsx
@@ -2,14 +2,8 @@ import React, {FC} from 'react';
 import BgCard from "../bgCard";
 import moment from "moment/moment";
 import {IPost, IUser} from "../../types/types";
-import {RiDeleteBin6Fill, RiEditFill} from "react-icons/ri";
-import {MdVisibility, MdVisibilityOff} from "react-icons/md";
-import PostForm from "../admin/PostForm";
-import axios from "axios";
 import {BiTime} from "react-icons/bi";
-import {deletePost, editPost} from "../../store/reducers/blogSlice";
-import {setModal} from "../../store/reducers/modalSlice";
-import {useAppDispatch} from "../../hooks/redux";
+import PostCardAdmin from "./PostCard__admin";
 
 
 interface PostCardProps {
@@ -18,62 +12,23 @@ interface PostCardProps {
 }
 
 const PostCard: FC<PostCardProps> = ({posts, user}) => {
-    const dispatch = useAppDispatch()
-    const editPostHandler = (post) => {
-        dispatch(setModal({
-            title: 'Редактирование',
-            children: <PostForm post={post}/>
-        }))
-    }
-    const visibilityChange = (id) => {
-        axios.post('/admin/visibilityBlogPost', {id})
-            .then((r) => {
-                dispatch(editPost(r.data))
-            })
-            .catch(err => alert(err))
-    };
-    const deletePostHandler = (post) => {
-        axios.post('/admin/deleteBlogPost', {id: post.id})
-            .then(() => {
-                dispatch(deletePost(post))
-            })
-            .catch(err => alert(err))
-    };
 
     if (posts) return (
         <>
             {posts?.map(post => (post.visibility === 0 || user?.id === 1) &&
                 <BgCard key={post.id}
-                        className={'min-h-[250px] group drop-shadow-xl sm:flex-col xs:justify-start'}>
+                        className={'h-[250px] group drop-shadow-xl sm:flex-col xs:justify-start'}>
                     {user?.id == 1 &&
-                        <div className={'flex group-hover:opacity-100 opacity-0 transition-all flex-col absolute gap-2 right-0 mr-4 text-xl'}>
-                            <span className={'flex hover:scale-125 cursor-pointer'}
-                                  onClick={() => editPostHandler(post)}>
-                                <RiEditFill/>
-                            </span>
-                            <span className={'flex hover:scale-125 cursor-pointer'}
-                                  onClick={() => visibilityChange(post.id)}
-                            >
-                                {!post.visibility ?
-                                    <MdVisibilityOff/>
-                                    :
-                                    <MdVisibility/>
-                                }
-                                </span>
-                            <span className={'flex hover:scale-125 cursor-pointer text-rose-500'}
-                                  onClick={() => deletePostHandler(post)}
-                            >
-                                <RiDeleteBin6Fill/>
-                            </span>
-                        </div>}
+                        <PostCardAdmin post={post}/>
+                    }
                     <div className={'flex justify-between gap-2'}>
                         <span
                             className={'font-bold overflow-hidden overflow-ellipsis whitespace-nowrap'}>
                     {post.title}
                         </span>
                     </div>
-                    <div className={'flex-1'}>
-                        {post.body}
+                    <div className={'flex-1 overflow-ellipsis'}>
+                        {post.body.substring(0, 196) + '....'}
                     </div>
                     <div className={'flex justify-end'}>
                         <span className={'flex items-center gap-1'}>
---
+++ resources/js/components/blog/PostCard__admin.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import {RiDeleteBin6Fill, RiEditFill} from "react-icons/ri";
+import {MdVisibility, MdVisibilityOff} from "react-icons/md";
+import {setModal} from "../../store/reducers/modalSlice";
+import PostForm from "../admin/PostForm";
+import axios from "axios";
+import {deletePost, editPost} from "../../store/reducers/blogSlice";
+import {useAppDispatch} from "../../hooks/redux";
+
+const PostCardAdmin = ({post}) => {
+    const dispatch = useAppDispatch()
+    const editPostHandler = (post) => {
+        dispatch(setModal({
+            title: 'Редактирование',
+            children: <PostForm post={post}/>
+        }))
+    }
+    const visibilityChange = (id) => {
+        axios.post('/admin/visibilityBlogPost', {id})
+            .then((r) => {
+                dispatch(editPost(r.data))
+            })
+            .catch(err => alert(err))
+    };
+    const deletePostHandler = (post) => {
+        axios.post('/admin/deleteBlogPost', {id: post.id})
+            .then(() => {
+                dispatch(deletePost(post))
+            })
+            .catch(err => alert(err))
+    };
+    return (
+        <div className={'flex group-hover:opacity-100 opacity-0 transition-all flex-col absolute gap-2 right-0 mr-4 text-xl'}>
+                            <span className={'flex hover:scale-125 cursor-pointer'}
+                                  onClick={() => editPostHandler(post)}>
+                                <RiEditFill/>
+                            </span>
+            <span className={'flex hover:scale-125 cursor-pointer'}
+                  onClick={() => visibilityChange(post.id)}
+            >
+                                {!post.visibility ?
+                                    <MdVisibilityOff/>
+                                    :
+                                    <MdVisibility/>
+                                }
+                                </span>
+            <span className={'flex hover:scale-125 cursor-pointer text-rose-500'}
+                  onClick={() => deletePostHandler(post)}
+            >
+                                <RiDeleteBin6Fill/>
+                            </span>
+        </div>
+    );
+};
+
+export default PostCardAdmin;
