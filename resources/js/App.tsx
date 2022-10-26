69922fec20809bc5c096971cfe2c0dfda9c470f7
nesa402610 <argyleferris@gmail.com>
2022-10-26 06:32:58 +0300

    Асинхронно получаем в стор данные с ошибкой))

--- resources/js/App.tsx
+++ resources/js/App.tsx
@@ -2,25 +2,20 @@ import React, {useEffect} from 'react';
 import Header from "./components/header";
 import './App.css'
 import './API/axios'
-import axios from "axios";
 import Modal from "./components/UI/modal";
 import Index from "./routes";
-import {login} from "./store/reducers/authSlice";
-import {useAppDispatch, useAppSelector} from "./hooks/redux";
-import {setProjects} from "./store/reducers/homePageSlice";
+import {AppDispatch, useAppSelector} from "./hooks/redux";
+import {fetchUser} from "./store/asyncActions/userActionsCreators";
+import {fetchProjects} from "./store/asyncActions/projectActionCreators";
+import {useDispatch} from "react-redux";
 
 const App = () => {
-    const dispatch = useAppDispatch()
+    const dispatch = useDispatch<AppDispatch>()
     const modal = useAppSelector(state => state.modal)
 
     useEffect(() => {
-        axios.get('/projects').then(r => dispatch(setProjects(r.data)))
-        axios.post('/checkLogin')
-            .then(r => {
-                dispatch(login(r.data))
-            }).catch(() => {
-            // dispatch(logoutAction(null))
-        })
+        dispatch(fetchUser())
+        dispatch(fetchProjects())
     }, [])
     return (
         <>
