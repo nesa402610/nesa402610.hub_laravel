d0c18b151de531b1ffe7020bca8adc906795379f
nesa402610 <argyleferris@gmail.com>
2022-10-28 20:45:31 +0300

    компонент написания и отправки коммента

---
+++ resources/js/routes/Blog/CreateComment.tsx
@@ -0,0 +1,48 @@
+import React, {FC, useState} from 'react';
+import BgCard from "../../components/bgCard";
+import SubmitButton from "../../components/UI/submitButton";
+import {useAppDispatch, useAppSelector} from "../../hooks/redux";
+import {IPost} from "../../types/types";
+import axios from "axios";
+import {addComment} from "../../store/reducers/blogSlice";
+
+interface CreateCommentProps {
+    handler?: any
+    post: IPost
+}
+
+const CreateComment: FC<CreateCommentProps> = ({post}) => {
+    const {isAuth} = useAppSelector(state => state.auth)
+    const [comment, setComment] = useState('');
+    const dispatch = useAppDispatch()
+
+    const createCommentHandler = (e) => {
+        e.preventDefault()
+    if (comment.length>=10) {
+        axios.post('blog/commentCreate', {post_id: post.id, body: comment})
+            .then(r => {
+                dispatch(addComment(r.data))
+                console.log(post.comments)
+            })
+            .catch(err=>alert(err))
+    }
+    }
+    return (
+        <>
+            {isAuth && post &&
+                <BgCard className={'sm:flex-col'}>
+                   <textarea className={'invalid:border-red-700 invalid:border p-4 bg-stone-500 rounded-lg w-full outline-none'} minLength={10}
+                             placeholder={'Оставить комментарий'}
+                             onChange={e => setComment(e.target.value)}
+                             value={comment}
+                   />
+                    <SubmitButton className={'self-end bg-stone-500 hover:bg-stone-700 px-6'}
+                                  onClick={e => createCommentHandler(e)}
+                    >Отправить</SubmitButton>
+                </BgCard>
+            }
+        </>
+    );
+};
+
+export default CreateComment;
