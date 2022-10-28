b3a5c08dba36763f6c8465bdf2828479117a8f09
nesa402610 <argyleferris@gmail.com>
2022-10-28 20:42:55 +0300

    Отношение постов к комментам

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class BlogPost extends Model
 {
     use HasFactory;
+
+    public function comments() {
+        return $this->hasMany(Comment::class, 'post_id');
+    }
 }
