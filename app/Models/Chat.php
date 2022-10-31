95753facfd6df5e5b4c4dabe7c0fa30bdac7164f
nesa402610 <argyleferris@gmail.com>
2022-10-31 11:47:40 +0300

    Выборка полей

--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -10,6 +10,6 @@ class Chat extends Model
     use HasFactory;
 
     public function user() {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class)->select('name', 'avatar');
     }
 }
