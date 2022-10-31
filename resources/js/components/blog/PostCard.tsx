8b10547c8b4e35d0bdad367d87c6d60259a9176f
nesa402610 <argyleferris@gmail.com>
2022-10-31 23:30:33 +0300

    Фикс обрезки текста

--- resources/js/components/blog/PostCard.tsx
+++ resources/js/components/blog/PostCard.tsx
@@ -18,26 +18,29 @@ const PostCard: FC<PostCardProps> = ({posts, user}) => {
             {posts.map(post => (post.visibility === 0 || user?.id === 1) &&
                 <Link to={`${post.id}`} key={post.id}>
                     <BgCard
-                           className={'h-[250px] transition-all cursor-pointer hover:-translate-y-2 group drop-shadow-xl sm:flex-col xs:justify-start'}>
-                    {user?.id == 1 &&
-                        <PostCardAdmin post={post}/>
-                    }
-                    <div className={'flex justify-between gap-2'}>
+                        className={'h-[250px] transition-all cursor-pointer hover:-translate-y-2 group drop-shadow-xl sm:flex-col xs:justify-start'}>
+                        {user?.id == 1 &&
+                            <PostCardAdmin post={post}/>
+                        }
+                        <div className={'flex justify-between gap-2'}>
                         <span
                             className={'font-bold overflow-hidden overflow-ellipsis whitespace-nowrap'}>
                     {post.title}
                         </span>
-                    </div>
-                    <div className={'flex-1 overflow-ellipsis'}>
-                        {post.body.substring(0, 196) + '....'}
-                    </div>
-                    <div className={'flex justify-end'}>
+                        </div>
+                        <div className={'flex-1 overflow-ellipsis'}>
+                            {post.body.length > 180 ?
+                                post.body.substring(0, 180) + '....'
+                                : post.body
+                            }
+                        </div>
+                        <div className={'flex justify-end'}>
                         <span className={'flex items-center gap-1'}>
                             {moment(post.created_at).format('DD.MM.YYYY')}
                             <BiTime className={'text-lg'}/>
                         </span>
-                    </div>
-                </BgCard></Link>
+                        </div>
+                    </BgCard></Link>
             )}
         </>
     );
