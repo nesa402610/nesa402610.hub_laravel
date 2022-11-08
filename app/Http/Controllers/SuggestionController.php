3b3615d692b98e8f356978849344b3e851f3bed2
nesa402610 <argyleferris@gmail.com>
2022-11-08 13:46:09 +0300

    Миграция, модель и контроллер для задач

---
+++ app/Http/Controllers/SuggestionController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Suggestion;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SuggestionController extends Controller {
+    public function getAll() {
+        $all = Suggestion::orderBy('id', 'desc')->get();
+        foreach ($all as $item) {
+            $item->user;
+        }
+        return response($all);
+    }
+
+    public function createSuggestion(Request $request) {
+        $sug = new Suggestion;
+
+        $sug->title = $request->title;
+        $sug->body = $request->body;
+        $sug->author = Auth::user()->id;
+        $sug->save();
+        return response($sug, 201);
+    }
+
+    public function updateSuggestion(Request $request) {
+        $sug = Suggestion::find($request->id);
+        $sug->title = $request->title;
+        $sug->body = $request->body;
+        $sug->save();
+
+    }
+
+    public function setStatus(Request $request) {
+        $sug = Suggestion::find($request->id);
+        if ($request->status === 666) return $sug->delete();
+        $sug->status = $request->status;
+        $sug->save();
+        return response($sug, 201);
+    }
+}
