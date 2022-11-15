f137b688ca686fc106cf06e04ee59f6cc88c619a
nesa402610 <argyleferris@gmail.com>
2022-11-15 13:31:38 +0300

    Будущий мини апсы

---
+++ resources/js/routes/miniApps/Timer/App.tsx
@@ -0,0 +1,11 @@
+import React, {FC} from 'react';
+
+const App: FC = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default App;
---
+++ resources/js/routes/miniApps/routing.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TimerApp from "./Timer/App";
+import {Route} from "react-router-dom";
+import MiniApps from "./index";
+
+const Routing = () => {
+    return (
+
+    );
+};
+
+export default Routing;
