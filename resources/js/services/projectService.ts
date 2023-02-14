407f225bd6f6e7c2e1f585ec19ff997fa40ae564
nesa402610 <argyleferris@gmail.com>
2023-02-14 21:37:55 +0300

    Добавление проекта на RTKq

--- resources/js/services/projectService.ts
+++ resources/js/services/projectService.ts
@@ -1,13 +1,32 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+import {IProject} from "../types/types";
+
+const csrf_token = document.querySelector("meta[name='csrf-token']").getAttribute("content");
 
 export const projectAPI = createApi({
     reducerPath: 'projectApi',
-    baseQuery: fetchBaseQuery({baseUrl: 'api'}),
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'api',
+        prepareHeaders: (headers) => {
+            headers.set('X-CSRF-TOKEN', csrf_token)
+            return headers
+        },
+    }),
+    tagTypes: ['Project'],
     endpoints: (builder) => ({
         getProjects: builder.query({
             query: () => `/projects`,
+            providesTags: ['Project']
         }),
+        createProject: builder.mutation({
+            query: (project: IProject) => ({
+                url: '/admin/createProject',
+                method: 'POST',
+                body: project
+            }),
+            invalidatesTags: ['Project']
+        })
     }),
 })
 
-export const {useGetProjectsQuery} = projectAPI
+export const {useGetProjectsQuery, useCreateProjectMutation} = projectAPI
