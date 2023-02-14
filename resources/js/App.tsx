8733633be0ccccfdc124c7c9347f40d866c222c5
nesa402610 <argyleferris@gmail.com>
2023-02-14 21:16:14 +0300

    Убираем redux selector

--- resources/js/App.tsx
+++ resources/js/App.tsx
@@ -2,15 +2,11 @@ import React from 'react';
 import Header from "./components/header";
 import './App.css'
 import './API/axios'
-import Modal from "./components/UI/modal";
 import Index from "./routes";
-import {useAppSelector} from "./hooks/redux";
 
 const App = () => {
-    const modal = useAppSelector(state => state.modal)
     return (
         <>
-            {modal.isModal && <Modal/>}
             <Header/>
             <Index/>
         </>
