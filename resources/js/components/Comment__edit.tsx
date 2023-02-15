15f2c709a1017ab74dcaa91ffcc689ef9f1e8e87
nesa402610 <argyleferris@gmail.com>
2023-02-15 19:58:03 +0300

    Работа с комментами

--- resources/js/components/Comment__edit.tsx
+++ resources/js/components/Comment__edit.tsx
@@ -1,32 +1,32 @@
 import React, {FC} from 'react';
 import {RiDeleteBin6Fill, RiEditFill} from "react-icons/ri";
-import axios from "axios";
+import {useDeleteCommentMutation} from "../services/postService";
 
 interface CommentEditProps {
-    id: number
-    setIsEdit: any
-    body: string
-    setComment: any
+  id: number
+  setIsEdit: any
+  body: string
+  setComment: any
 }
 
 const CommentEdit: FC<CommentEditProps> = ({id, setIsEdit, body, setComment}) => {
-    const editComment = (e, id) => {
-        setIsEdit(id)
-        setComment(body)
-        // axios.post('/blog/comments/edit')
-    }
-    const deleteComment = (e, id) => {
-        axios.post('/blog/comments/delete', {id})
-            .then(r => alert(r.data))
-    }
-    return (
-        <div className={'absolute right-0 mr-4 flex gap-2'}>
-            <span onClick={event => editComment(event, id)}
+  const [deleteComment, {}] = useDeleteCommentMutation()
+  const updateCommentHandler = () => {
+    setIsEdit(id)
+    setComment(body)
+    // axios.post('/blog/comments/edit')
+  }
+  const deleteCommentHandler = () => {
+    deleteComment({id})
+  }
+  return (
+    <div className={'absolute right-0 mr-4 flex gap-2'}>
+            <span onClick={updateCommentHandler}
                   className={'hover:scale-125 cursor-pointer'}><RiEditFill/></span>
-            <span onClick={e => deleteComment(e, id)}
-                  className={'hover:scale-125 cursor-pointer text-rose-500'}><RiDeleteBin6Fill/></span>
-        </div>
-    );
+      <span onClick={deleteCommentHandler}
+            className={'hover:scale-125 cursor-pointer text-rose-500'}><RiDeleteBin6Fill/></span>
+    </div>
+  );
 };
 
 export default CommentEdit;
