d9bad6d32d0f35dd72cc11ddddbbf9940c693834
nesa402610 <argyleferris@gmail.com>
2023-02-15 19:58:45 +0300

    На RTKq уводим все и перерабона работа с модалками

--- resources/js/components/admin/PostForm.tsx
+++ resources/js/components/admin/PostForm.tsx
@@ -1,65 +1,57 @@
-import React, {FC, useEffect, useState} from 'react';
+import React, {Dispatch, FC, SetStateAction, useState} from 'react';
 import {IPost} from "../../types/types";
-import axios from "axios";
-import {addPost, editPost} from "../../store/reducers/blogSlice";
-import {closeModal} from "../../store/reducers/modalSlice";
-import {useAppDispatch} from "../../hooks/redux";
+import Modal from "../UI/modal";
+import {useCreatePostMutation, useUpdatePostMutation} from "../../services/postService";
 
 interface PostFormProps {
-    post?: IPost
+  post?: IPost
+  isModal: boolean
+  setIsModal: Dispatch<SetStateAction<boolean>>
 }
 
-const PostForm: FC<PostFormProps> = ({post}) => {
-    const dispatch = useAppDispatch()
-    const [data, setData] = useState<IPost>({
-        title: '', body: '', image: ''
-    });
-    useEffect(() => {
-        if (post) setData(post)
-    }, [])
-    const createPost = (e) => {
-        e.preventDefault()
-        axios.post('/admin/createBlogPost', data)
-            .then(r => {
-                dispatch(addPost(r.data))
-            })
-            .then(() => dispatch(closeModal()))
-    }
+const PostForm: FC<PostFormProps> = ({post, isModal, setIsModal}) => {
+  const [createPost, {}] = useCreatePostMutation()
+  const [updatePost, {}] = useUpdatePostMutation()
+  const [data, setData] = useState<IPost>(post || {title: '', body: '', image: ''});
+  const createPostHandler = (e) => {
+    e.preventDefault()
+    createPost(data)
+    setIsModal(false)
+  }
+  const updatePostHandler = (e) => {
+    e.preventDefault()
+    updatePost(data)
+    setIsModal(false)
+  }
 
-    const updatePost = (e) => {
-        e.preventDefault()
-        axios.post('/admin/updateBlogPost', data)
-            .then(r => {
-                dispatch(editPost(r.data))
-            })
-            .then(() => dispatch(closeModal()))
-    };
-
-    return (
-        <div className={'block--light'}>
-            <div className={'block--dark flex flex-col gap-4'}>
-                <div className={'flex flex-col gap-4'}>
-                    <label>
-                        Название поста
-                        <input type={'text'}
-                               value={data.title}
-                               onChange={e => setData({...data, title: e.target.value})}/>
-                    </label>
-                    <label>
-                        Тело поста
-                        <input type={'text'}
-                               value={data.body}
-                               onChange={e => setData({...data, body: e.target.value})}/>
-                    </label>
-                </div>
-                {!post ?
-                    <button onClick={createPost}>Создать пост</button>
-                    :
-                    <button onClick={updatePost}>Обновить пост</button>
-                }
-            </div>
+  if (!isModal) return null;
+  return (
+    <Modal title={'Создание поста'} closeHandler={setIsModal}>
+      <div className={'block--light'}>
+        <div className={'block--dark flex flex-col gap-4'}>
+          <div className={'flex flex-col gap-4'}>
+            <label>
+              Название поста
+              <input type={'text'}
+                     value={data.title}
+                     onChange={e => setData({...data, title: e.target.value})}/>
+            </label>
+            <label>
+              Тело поста
+              <input type={'text'}
+                     value={data.body}
+                     onChange={e => setData({...data, body: e.target.value})}/>
+            </label>
+          </div>
+          {!post ?
+            <button onClick={createPostHandler}>Создать пост</button>
+            :
+            <button onClick={updatePostHandler}>Обновить пост</button>
+          }
         </div>
-    );
+      </div>
+    </Modal>
+  );
 };
 
 export default PostForm;
