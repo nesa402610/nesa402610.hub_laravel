eb9f66eb9614d95e9c1d87de4eece52fcea53ad1
nesa402610 <argyleferris@gmail.com>
2023-02-15 19:58:28 +0300

    На RTKq уводим все и перерабона работа с модалками

--- resources/js/components/blog/PostCard.tsx
+++ resources/js/components/blog/PostCard.tsx
@@ -1,47 +1,50 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 import moment from "moment/moment";
 import {IPost, IUser} from "../../types/types";
 import {BiTime} from "react-icons/bi";
 import PostCardAdmin from "./PostCard__admin";
 import {Link} from "react-router-dom";
+import PostForm from "../admin/PostForm";
 
 
 interface PostCardProps {
-    post: IPost
-    user: IUser
+  post: IPost
+  user: IUser
 }
 
 const PostCard: FC<PostCardProps> = ({post, user}) => {
-    return (
-        <>
-            <Link to={`${post.id}`} key={post.id}>
-                <div
-                    className={'block--light h-[250px] transition-all cursor-pointer hover:-translate-y-2 group drop-shadow-xl sm:flex-col xs:justify-start'}>
-                    {user?.id == 1 &&
-                        <PostCardAdmin post={post}/>
-                    }
-                    <div className={'flex justify-between gap-2'}>
+  const [isModal, setIsModal] = useState<boolean>(false);
+  return (
+    <>
+      <PostForm isModal={isModal} setIsModal={setIsModal} post={post}/>
+      <Link to={`${post.id}`} key={post.id}>
+        <div
+          className={'block--light h-[250px] transition-all cursor-pointer hover:-translate-y-2 group drop-shadow-xl sm:flex-col xs:justify-start'}>
+          {user?.id == 1 &&
+            <PostCardAdmin post={post} setIsModal={setIsModal}/>
+          }
+          <div className={'flex justify-between gap-2'}>
                         <span
-                            className={'font-bold overflow-hidden overflow-ellipsis whitespace-nowrap'}>
+                          className={'font-bold overflow-hidden overflow-ellipsis whitespace-nowrap'}>
                     {post.title}
                         </span>
-                    </div>
-                    <div className={'flex-1 overflow-ellipsis'}>
-                        {post.body.length > 180 ?
-                            post.body.substring(0, 180) + '....'
-                            : post.body
-                        }
-                    </div>
-                    <div className={'flex justify-end'}>
+          </div>
+          <div className={'flex-1 overflow-ellipsis'}>
+            {post.body.length > 180 ?
+              post.body.substring(0, 180) + '....'
+              : post.body
+            }
+          </div>
+          <div className={'flex justify-end'}>
                         <span className={'flex items-center gap-1'}>
                             {moment(post.created_at).format('DD.MM.YYYY')}
-                            <BiTime className={'text-lg'}/>
+                          <BiTime className={'text-lg'}/>
                         </span>
-                    </div>
-                </div>
-            </Link>
-        </>
-    );
+          </div>
+        </div>
+      </Link>
+    </>
+  );
 };
 
 export default PostCard;
--- resources/js/components/blog/PostCard__admin.tsx
+++ resources/js/components/blog/PostCard__admin.tsx
@@ -1,57 +1,42 @@
 import React from 'react';
 import {RiDeleteBin6Fill, RiEditFill} from "react-icons/ri";
 import {MdVisibility, MdVisibilityOff} from "react-icons/md";
-import {setModal} from "../../store/reducers/modalSlice";
-import PostForm from "../admin/PostForm";
-import axios from "axios";
-import {deletePost, editPost} from "../../store/reducers/blogSlice";
-import {useAppDispatch} from "../../hooks/redux";
+import {useChangeVisibilityMutation, useDeletePostMutation} from "../../services/postService";
 
-const PostCardAdmin = ({post}) => {
-    const dispatch = useAppDispatch()
-    const editPostHandler = (e, post) => {
-        e.preventDefault()
-        dispatch(setModal({
-            title: 'Редактирование',
-            children: <PostForm post={post}/>
-        }))
-    }
-    const visibilityChange = (id) => {
-        axios.post('/admin/visibilityBlogPost', {id})
-            .then((r) => {
-                dispatch(editPost(r.data))
-            })
-            .catch(err => alert(err))
-    };
-    const deletePostHandler = (post) => {
-        axios.post('/admin/deleteBlogPost', {id: post.id})
-            .then(() => {
-                dispatch(deletePost(post))
-            })
-            .catch(err => alert(err))
-    };
-    return (
-        <div className={'flex group-hover:opacity-100 opacity-0 transition-all flex-col absolute gap-2 right-0 mr-4 text-xl'}>
+const PostCardAdmin = ({post, setIsModal}) => {
+  const [deletePost, {}] = useDeletePostMutation()
+  const [changeVisibility, {}] = useChangeVisibilityMutation()
+  const editPostHandler = (e) => {
+    e.preventDefault()
+    setIsModal(true)
+
+  }
+  const visibilityChange = (e: React.MouseEvent<HTMLSpanElement, MouseEvent>, id: number) => {
+    e.preventDefault()
+    changeVisibility(id)
+  };
+  const deletePostHandler = (e: React.MouseEvent<HTMLSpanElement, MouseEvent>, id: number) => {
+    e.preventDefault()
+    deletePost(id)
+  };
+  return (
+    <div className={'flex group-hover:opacity-100 opacity-0 transition-all flex-col absolute gap-2 right-0 mr-4 text-xl'}>
             <span className={'flex hover:scale-125 cursor-pointer'}
-                  onClick={(e) => editPostHandler(e, post)}>
+                  onClick={(e) => editPostHandler(e)}>
                 <RiEditFill/>
             </span>
-            <span className={'flex hover:scale-125 cursor-pointer'}
-                  onClick={() => visibilityChange(post.id)}
-            >
+      <span className={'flex hover:scale-125 cursor-pointer'}
+            onClick={(e) => visibilityChange(e, post.id)}>
                 {!post.visibility ?
-                    <MdVisibilityOff/>
-                    :
-                    <MdVisibility/>
+                  <MdVisibilityOff/> : <MdVisibility/>
                 }
-            </span>
-            <span className={'flex hover:scale-125 cursor-pointer text-rose-500'}
-                  onClick={() => deletePostHandler(post)}
-            >
+      </span>
+      <span className={'flex hover:scale-125 cursor-pointer text-rose-500'}
+            onClick={(e) => deletePostHandler(e, post.id)}>
                 <RiDeleteBin6Fill/>
-            </span>
-        </div>
-    );
+      </span>
+    </div>
+  );
 };
 
 export default PostCardAdmin;
