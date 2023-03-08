62cf3d3e2d7398b80179c17e11f6a8fd54d23cc9
nesa402610 <argyleferris@gmail.com>
2023-03-08 22:39:10 +0300

    Декомпозиция

---
+++ resources/js/components/collection/Collection.tsx
@@ -0,0 +1,52 @@
+import React, {FC, useEffect} from 'react';
+import {ICollection} from "../../types/types";
+import {useLazyGetCollectionQuery} from "../../services/HCollectionService";
+import Loader from "../Loader";
+
+
+interface CollectionProps {
+  passkey: string
+}
+
+const Collection: FC<CollectionProps> = ({passkey}) => {
+  const [trigger, {data: collection, isLoading, error}] = useLazyGetCollectionQuery()
+
+  useEffect(() => {
+    if (passkey.length === 18) {
+      trigger(passkey)
+    }
+  }, [passkey]);
+  if (!collection) return <Loader/>
+  return (
+    <>
+      {collection.map((title: ICollection) =>
+        <div key={title.id} className={'bg-neutral-700 p-4 rounded-lg'}>
+          <div className={'flex xs:flex-col md:flex-row gap-4'}>
+            <img className={'rounded-lg w-[200px] h-fit self-center'} src={title.image} alt="Изображение тайтла"/>
+            <div className={'flex flex-col'}>
+              <span>{title.title_ru} / {title.title_original}</span>
+              {title.links.map((link, index) => <a key={link.link}
+                                                   href={link.link}
+                                                   target={"_blank"}>Ссылка {index + 1}</a>)}
+              <div className={'text-neutral-300'}>
+                <h3 className={'mt-4 font-bold'}>Информация</h3>
+                <div className={'flex flex-col'}>
+                  <span>Год выпуска: {title.announce_date}</span>
+                  <span>Жарны: {title.tags.map(tag => tag.name + ' ')}</span>
+                  <span>Эпизоды: {title.episodes}</span>
+                  <span>Длительность серии: {title.episode_time}</span>
+                  <span>Студия: {title.studio}</span>
+                  <span>Цензура: {title.censure ? 'С цензурой' : 'Без цензуры'}</span>
+                </div>
+                <h3 className={'mt-4 font-bold'}>Описание</h3>
+                <p>{title.description}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Collection;
--- resources/js/routes/HCollection.tsx
+++ resources/js/routes/HCollection.tsx
@@ -1,7 +1,6 @@
 import React, {FC, useEffect, useState} from 'react';
 import {useLazyGetCollectionQuery} from "../services/HCollectionService";
-import {ICollection} from "../types/types";
-import Loader from "../components/Loader";
+import Collection from "../components/collection/Collection";
 
 const HCollection: FC = () => {
   const [trigger, {data: collection, isLoading, error}] = useLazyGetCollectionQuery()
@@ -18,11 +17,7 @@ const HCollection: FC = () => {
     }
   }, [collection]);
 
-  useEffect(() => {
-    if (passkey.length === 18) {
-      trigger(passkey)
-    }
-  }, [passkey]);
+
   if (!localStorage.getItem('passkey')) {
     return (
       <div className={'block--dark flex flex-col m-4'}>
@@ -43,31 +38,7 @@ const HCollection: FC = () => {
         <h3 className={'text-sm text-neutral-500 italic text-end mr-2'}>
           Я не при делах если что. Все данные взяты с открытых источников.
         </h3>
-        {collection ?
-          collection?.map((title: ICollection) =>
-            <div key={title.id} className={'bg-neutral-700 p-4 rounded-lg'}>
-              <div className={'flex gap-4'}>
-                <img className={'rounded-lg'} src={title.image} width={250} alt="Изображение тайтла"/>
-                <div className={'flex flex-col'}>
-                  <span>{title.title_ru} / {title.title_original}</span>
-                  {title.links.map((link, index) => <a href={link.link}>Ссылка {index + 1}</a>)}
-                  <div className={'text-neutral-300'}>
-                    <h3 className={'mt-4 font-bold'}>Информация</h3>
-                    <div className={'flex flex-col'}>
-                      <span>Год выпуска: {title.announce_date}</span>
-                      <span>Жарны: {title.tags.map(tag => tag.name)}</span>
-                      <span>Эпизоды: {title.episodes}</span>
-                      <span>Длительность серии: {title.episode_time}</span>
-                      <span>Студия: {title.studio}</span>
-                      <span>Цензура: {title.censure ? 'С цензурой' : 'Без цензуры'}</span>
-                    </div>
-                    <h3 className={'mt-4 font-bold'}>Описание</h3>
-                    <p>{title.description}</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ) : <Loader/>}
+        <Collection passkey={passkey}/>
       </div>
     </div>
   );
