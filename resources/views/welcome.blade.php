23faa7be454ee9b3392db88f8db8324574027812
nesa402610 <argyleferris@gmail.com>
2023-03-15 23:00:15 +0300

    Тема сайта изменена

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
     <link rel="manifest" href="/site.webmanifest">
     <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="theme-color" content="#44403c">
+    <meta name="theme-color" content="rgb(38 38 38)">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>n/esa | hub</title>
     @viteReactRefresh
