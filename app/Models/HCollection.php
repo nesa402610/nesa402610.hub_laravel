320d7e945bc03da61b174c893fb159229f3d020c
nesa402610 <argyleferris@gmail.com>
2023-03-16 10:42:21 +0300

    Добавлен tag_id к выдаче

--- app/Models/HCollection.php
+++ app/Models/HCollection.php
@@ -10,7 +10,7 @@ class HCollection extends Model
     use HasFactory;
 
     public function tags() {
-        return $this->belongsToMany(Tags::class, 'tag_collection', 'collection_id', 'tag_id')->select(['name']);
+        return $this->belongsToMany(Tags::class, 'tag_collection', 'collection_id', 'tag_id')->select(['tag_id', 'name']);
     }
     public function links() {
         return $this->hasMany(HLinks::class, 'collection_id')->select('link');
