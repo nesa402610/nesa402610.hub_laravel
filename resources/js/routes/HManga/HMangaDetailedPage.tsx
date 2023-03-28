e42778eb15688fc46379ea41dc9b3e2e1256760d
nesa402610 <argyleferris@gmail.com>
2023-03-28 18:56:46 +0300

    Манга декомпозирована

--- resources/js/routes/HManga/HMangaDetailedPage.tsx
+++ resources/js/routes/HManga/HMangaDetailedPage.tsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, {useState} from "react";
+import {useParams} from "react-router";
+import {useGetAllMangaQuery} from "../../services/HCollectionService";
+import HCollectionCard from "../../components/HCollection/HCollectionCard";
+import Modal from "../../components/UI/modal";
 
 const HMangaDetailedPage = () => {
+  const passkey = localStorage.getItem("passkey");
+  const [page, setPage] = useState({page: null, link: ""});
+  const [isModal, setIsModal] = useState(false);
+  const {id} = useParams();
+  const {data} = useGetAllMangaQuery({passkey}, {
+    selectFromResult: ({data}) => ({
+      data: data?.collections.data.find(d => d.id == id),
+    })
+  });
+
+  const viewHandler = (page, link) => {
+    setPage({...page, page: page});
+    setPage({...page, link: link});
+    setIsModal(true);
+
+  };
+
   return (
-    <div>
-      
-    </div>
+    <>
+      {isModal &&
+        <Modal title={""} closeHandler={setIsModal}>
+          <img className="p-4 max-h-screen" src={page.link} alt=""/>
+        </Modal>}
+      <div className={"m-4 flex flex-col gap-4"}>
+        <HCollectionCard collection={data}/>
+        <div className={"flex gap-8 bg-neutral-700 rounded-lg p-4"}>
+          {data?.pages.map(p =>
+            <div key={p.id} className={"w-[200px]"}>
+              <img src={p.page_link} onClick={() => viewHandler(p.page, p.page_link)} alt=""/>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
   );
 };
 
--- resources/js/routes/HManga/HMangaPage.tsx
+++ resources/js/routes/HManga/HMangaPage.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React from "react";
+import HCollectionCard from "../../components/HCollection/HCollectionCard";
+import {useGetAllMangaQuery} from "../../services/HCollectionService";
+import Loader from "../../components/Loader";
+import {useOutletContext} from "react-router-dom";
+import {filterCollection} from "../HHHPage";
 
 const HMangaPage = () => {
+  const filter = useOutletContext()
+  const passkey = localStorage.getItem("passkey");
+  const {data, isLoading, error} = useGetAllMangaQuery({passkey});
+  const filteredCollection = filterCollection(data, filter)
+  if (isLoading) return <Loader/>;
   return (
-    <div className={'m-4'}>
-
+    <div className={"block--dark flex flex-col -mx-4 gap-4"}>
+      {filteredCollection?.map(collectionItem =>
+        <HCollectionCard key={collectionItem.id}
+                         collection={collectionItem}/>
+      )}
     </div>
   );
 };
