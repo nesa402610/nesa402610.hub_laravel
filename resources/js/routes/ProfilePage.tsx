8dbcda097948629d4d19e48f8133a12552feaccc
nesa402610 <argyleferris@gmail.com>
2023-03-30 00:40:45 +0300

    Фикс профиля

--- resources/js/routes/ProfilePage.tsx
+++ resources/js/routes/ProfilePage.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
 import {IoSettingsSharp} from "react-icons/io5";
 import {IUser} from "../types/types";
@@ -6,65 +6,66 @@ import moment from "moment";
 import {useParams} from "react-router";
 import axios from "axios";
 import {useGetUserQuery} from "../services/userService";
+import Loader from "../components/Loader";
 
 const ProfilePage = () => {
-    const {data:authedUser} = useGetUserQuery('')
-    const [user, setUser] = useState<IUser>(null)
-    const userName = useParams()
+  const {data: authedUser} = useGetUserQuery("");
+  const [user, setUser] = useState<IUser>(null);
+  const {username} = useParams();
 
-    useEffect(() => {
-        // @ts-ignore
-        if (authedUser?.id == userName.username || !userName) {
-            return setUser(authedUser)
-        } else {
-            axios.get('/profile/' + userName.username)
-                .then(r => {
-                    setUser(r.data)
-                })
-        }
+  useEffect(() => {
+    if (authedUser?.id == username || !username) {
+      return setUser(authedUser);
+    } else {
+      axios.get("/profile/" + username)
+        .then(r => {
+          setUser(r.data);
+        });
+    }
 
-    }, [userName])
+  }, [username]);
 
+  if (!user) return <Loader/>;
 
-    if (user) return (
-        <div className={'p-4'}>
-            <div className={'block--light'}>
-                <div className={'flex sm:flex-row xs:flex-col gap-4 w-full'}>
-                    <div className={'flex xs:items-center flex-col gap-2 sm:items-end'}>
-                        <img className={'rounded-lg'}
-                             width={'200px'}
-                             height={'200px'}
-                             src={user.avatar}
-                             alt="user profile picture"/>
-                        <Link to={'edit'}
-                              className={'hover:text-stone-400 transition-colors flex gap-2 text-2xl items-center'}>
-                            <span className={'text-lg'}>Настройки</span>
-                            <IoSettingsSharp/>
-                        </Link>
-                    </div>
-                    <div className={'flex flex-col gap-8 flex-1'}>
-                        <div>
-                            <div>
-                                {user.name} {user.lastName} {user.middleName}
-                            </div>
-                            <div>
-                                {user.birthday &&
-                                    (moment(user.birthday).format('YYYY')) + ' года / '}
-                                {user.status && user.status + ' / '} Здесь
-                                                                     с {moment(user.created_at).format('YYYY')} года
-                            </div>
-                        </div>
-                        <div className={'flex flex-col flex-1'}>
-                            <h2 className={'font-bold mb-2 italic text-end mr-2'}>Информация обо мне</h2>
-                            <div className={'block--dark flex-1'}>
-                                {user.about}
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className={"p-4"}>
+      <div className={"block--light"}>
+        <div className={"flex sm:flex-row xs:flex-col gap-4 w-full"}>
+          <div className={"flex xs:items-center flex-col gap-2 sm:items-end"}>
+            <img className={"rounded-lg"}
+                 width={"200px"}
+                 height={"200px"}
+                 src={user.avatar}
+                 alt="user profile picture"/>
+            {user.id == username && <Link to={"edit"}
+                                          className={"hover:text-stone-400 transition-colors flex gap-2 text-2xl items-center"}>
+              <span className={"text-lg"}>Настройки</span>
+              <IoSettingsSharp/>
+            </Link>}
+          </div>
+          <div className={"flex flex-col gap-8 flex-1"}>
+            <div>
+              <div>
+                {user.name} {user.lastName} {user.middleName}
+              </div>
+              <div>
+                {user.birthday &&
+                  (moment(user.birthday).format("YYYY")) + " года / "}
+                {user.status && user.status + " / "} Здесь
+                                                     с {moment(user.created_at).format("YYYY")} года
+              </div>
             </div>
+            <div className={"flex flex-col flex-1"}>
+              <h2 className={"font-bold mb-2 italic text-end mr-2"}>Информация обо мне</h2>
+              <div className={"block--dark flex-1"}>
+                {user.about}
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ProfilePage;
