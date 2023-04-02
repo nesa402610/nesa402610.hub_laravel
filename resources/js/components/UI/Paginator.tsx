055e40503efe442e751505c24a693b4a5689c456
nesa402610 <argyleferris@gmail.com>
2023-04-02 17:09:52 +0300

    Обновли логику пагинации, стала проще

--- resources/js/components/UI/Paginator.tsx
+++ resources/js/components/UI/Paginator.tsx
@@ -3,39 +3,19 @@ import {FaAngleDoubleLeft, FaAngleLeft, FaAngleRight} from "react-icons/fa";
 
 const Paginator = ({currentPage, setCurrentPage, totalPages, handler}) => {
   const passkey = localStorage.getItem("passkey");
-  const nextPageHandler = () => {
-    if (currentPage < totalPages) {
-      handler({passkey: passkey, page: currentPage + 1});
-      setCurrentPage(p=>p+1)
-    }
-  };
   const toPageHandler = (page) => {
     handler({passkey: passkey, page: page});
     setCurrentPage(page)
 
-  };
-  const prevPageHandler = (first?: boolean) => {
-    if (first) {
-      if (currentPage > 1) {
-        handler({passkey: passkey, page: 1});
-        setCurrentPage(1)
-      }
-
-    } else {
-      if (currentPage > 1) {
-        handler({passkey: passkey, page: currentPage - 1});
-        setCurrentPage(p=>p-1)
-      }
-    }
   };
   const pages = Array.from({length: totalPages}, (v, k) => k + 1);
   if (totalPages === 1) return null;
   return (
     <div className={"flex gap-4 items-center justify-center"}>
       <span className={currentPage === 1 ? "text-neutral-500" : "cursor-pointer hover:text-neutral-500 text-neutral-300"}
-            onClick={() => prevPageHandler(true)}><FaAngleDoubleLeft/></span>
+            onClick={() => toPageHandler(1)}><FaAngleDoubleLeft/></span>
       <span className={currentPage < totalPages ? "text-neutral-500" : "cursor-pointer hover:text-neutral-500 text-neutral-300"}
-            onClick={() => prevPageHandler()}><FaAngleLeft/></span>
+            onClick={() => toPageHandler(currentPage-1)}><FaAngleLeft/></span>
       {pages.map(page =>
         <span className={currentPage !== page ? "cursor-pointer hover:text-neutral-500 text-neutral-300" : "text-neutral-500"}
               onClick={() => toPageHandler(page)}>
@@ -43,7 +23,7 @@ const Paginator = ({currentPage, setCurrentPage, totalPages, handler}) => {
         </span>
       )}
       <span className={currentPage >= totalPages ? "text-neutral-500" : "cursor-pointer hover:text-neutral-500 text-neutral-300"}
-            onClick={nextPageHandler}><FaAngleRight/></span>
+            onClick={()=>toPageHandler(currentPage+1)}><FaAngleRight/></span>
     </div>
   );
 };
