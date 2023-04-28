ec4655f08885434cccba5474f957d6b9ceb7b981
nesa402610 <argyleferris@gmail.com>
2023-04-28 10:53:13 +0300

    Убрано лишнее

--- resources/js/components/HCollection/HCollectionTags.tsx
+++ resources/js/components/HCollection/HCollectionTags.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import { useRemoveTagMutation } from '../../services/Collections/HCollectionService';
+import React from "react";
 import {useGetUserQuery} from "../../services/userService";
 
 const HCollectionTags = ({collection, removeTag}) => {
-  const {data: user} = useGetUserQuery('')
+    const {data: user} = useGetUserQuery("");
 
-  const deleteTagHandler = (titleId, tagId) => {
-    removeTag({titleId, tagId})
-  }
+    const deleteTagHandler = (titleId, tagId) => {
+        removeTag({titleId, tagId});
+    };
   return (
     <>
       {collection.tags?.map(tag =>
-        <div key={tag.tag_id}
-             className={(tag.name === 'Золото' ? 'bg-amber-400 text-neutral-600' : tag.name === 'Серебро' ? 'bg-zinc-500' : tag.name === 'Медь' ? 'bg-amber-700' : 'bg-neutral-800') + '  px-2 rounded-full flex items-center'}>
-          <span className={'whitespace-nowrap'}>{tag.name}</span>
-          {user?.id === 1 &&
-            <span onClick={() => deleteTagHandler(collection.id, tag.tag_id)}
-                  className={'cursor-pointer ml-2 px-1 bg-neutral-700 text-white rounded-full leading-none bg-neutral-700 text-inherit'}>
+          <div key={tag.tag_id}
+               className={(tag.name === "Золото" ? "bg-amber-400 text-neutral-600" : tag.name === "Серебро" ? "bg-zinc-500" : tag.name === "Медь" ? "bg-amber-700" : "bg-neutral-800") + "  px-2 rounded-full flex items-center"}>
+              <span className={"whitespace-nowrap"}>{tag.name}</span>
+              {user?.id === 1 &&
+                  <span onClick={() => deleteTagHandler(collection.id, tag.tag_id)}
+                        className={"cursor-pointer ml-2 px-1 bg-neutral-700 text-white rounded-full leading-none text-inherit"}>
                               -
                             </span>
-          }
-        </div>
+              }
+          </div>
       )}
     </>
   );
