e6b69f0649c1b7d0ce4d1900eccda880dfe0d548
nesa402610 <argyleferris@gmail.com>
2023-05-12 12:14:39 +0300

    Пока юзера не получим не показываем страницу

--- resources/js/App.tsx
+++ resources/js/App.tsx
@@ -4,17 +4,21 @@ import "./App.css";
 import "./API/axios";
 import {Outlet} from "react-router";
 import {ScrollRestoration} from "react-router-dom";
+import {useGetUserQuery} from "./services/userService";
+import Loader from "./components/Loader";
 
 const App = () => {
-  return (
-    <>
-      <Header/>
-      <main className={"ml-[80px]"}>
-        <Outlet/>
-        <ScrollRestoration/>
-      </main>
-    </>
-  );
+    const {data, isFetching, error} = useGetUserQuery()
+    if (isFetching && !error) return <Loader/>
+    return (
+        <>
+            <Header/>
+            <main className={"ml-[80px]"}>
+                <Outlet/>
+                <ScrollRestoration/>
+            </main>
+        </>
+    );
 };
 
 export default App;
