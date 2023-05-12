a20008eb99f80fcae85ff88cfc7fa48158d6db19
nesa402610 <argyleferris@gmail.com>
2023-05-12 12:16:09 +0300

    Теперь можно центрированным сделать по всему экрану

--- resources/js/components/Loader.tsx
+++ resources/js/components/Loader.tsx
@@ -2,16 +2,17 @@ import React, {FC} from "react";
 import {RiLoader3Fill} from "react-icons/ri";
 
 interface LoaderProps {
-  text?: string
+    text?: string
+    center?: boolean
 }
 
-const Loader: FC<LoaderProps> = ({text = 'Загрузка'}) => {
-  return (
-    <div className={'m-4 flex justify-center items-center'}>
-      <h2 className={'text-center font-bold text-xl'}>{text}...</h2>
-      <RiLoader3Fill size={'2rem'} className={'animate-spin'}/>
-    </div>
-  );
+const Loader: FC<LoaderProps> = ({text = 'Загрузка', center = false}) => {
+    return (
+        <div className={`m-4 flex justify-center items-center ${center ? 'justify-center h-full' : ''}`}>
+            <h2 className={'text-center font-bold text-xl'}>{text}...</h2>
+            <RiLoader3Fill size={'2rem'} className={'animate-spin'}/>
+        </div>
+    );
 };
 
 export default Loader;
