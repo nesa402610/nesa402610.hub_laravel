1962820a556730ab84e0e49deb20e35f909451f2
nesa402610 <argyleferris@gmail.com>
2023-05-15 15:37:24 +0300

    Жесткий фикс визуала

--- resources/js/components/UI/Paginator.tsx
+++ resources/js/components/UI/Paginator.tsx
@@ -1,31 +1,42 @@
 import React from "react";
-import {FaAngleDoubleLeft, FaAngleLeft, FaAngleRight} from "react-icons/fa";
+import {FaAngleDoubleLeft, FaAngleDoubleRight, FaAngleLeft, FaAngleRight} from "react-icons/fa";
 
-const Paginator = ({currentPage, setCurrentPage, totalPages, handler}) => {
-  const passkey = localStorage.getItem("passkey");
-  const toPageHandler = (page) => {
-    handler({passkey: passkey, page: page});
-    setCurrentPage(page)
+const Paginator = ({currentPage, setCurrentPage, totalPages}) => {
+    const toPageHandler = (page) => {
+        setCurrentPage(page)
 
-  };
-  const pages = Array.from({length: totalPages}, (v, k) => k + 1);
-  if (totalPages === 1) return null;
-  return (
-    <div className={"flex gap-4 items-center justify-center"}>
-      <span className={currentPage === 1 ? "text-neutral-500" : "cursor-pointer hover:text-neutral-500 text-neutral-300"}
-            onClick={() => toPageHandler(1)}><FaAngleDoubleLeft/></span>
-      <span className={currentPage < totalPages ? "text-neutral-500" : "cursor-pointer hover:text-neutral-500 text-neutral-300"}
-            onClick={() => toPageHandler(currentPage-1)}><FaAngleLeft/></span>
-      {pages.map(page =>
-        <span className={currentPage !== page ? "cursor-pointer hover:text-neutral-500 text-neutral-300" : "text-neutral-500"}
-              onClick={() => toPageHandler(page)}>
-          {page}
-        </span>
-      )}
-      <span className={currentPage >= totalPages ? "text-neutral-500" : "cursor-pointer hover:text-neutral-500 text-neutral-300"}
-            onClick={()=>toPageHandler(currentPage+1)}><FaAngleRight/></span>
-    </div>
-  );
+    };
+    const pages = Array.from({length: totalPages}, (v, k) => k + 1);
+    if (totalPages === 1) return null;
+    return (
+        <div className={"flex gap-4 items-center justify-center"}>
+      <span
+          className={currentPage === 1 ? "text-neutral-500" : "cursor-pointer hover:text-neutral-500 text-neutral-300"}
+          onClick={() => toPageHandler(1)}>
+          <FaAngleDoubleLeft/>
+      </span>
+            <span
+                className={currentPage === 1 ? "text-neutral-500" : "cursor-pointer hover:text-neutral-500 text-neutral-300"}
+                onClick={() => toPageHandler(currentPage - 1)}><FaAngleLeft/></span>
+            {pages.map(page =>
+                <span
+                    onClick={() => toPageHandler(page)}
+                    className={currentPage !== page ? "cursor-pointer hover:text-neutral-500 text-neutral-300" : "text-neutral-500"}>
+                      {page}
+                    </span>
+            )}
+            <span
+                className={currentPage >= totalPages ? "text-neutral-500" : "cursor-pointer hover:text-neutral-500 text-neutral-300"}
+                onClick={() => toPageHandler(currentPage + 1)}>
+                 <FaAngleRight/>
+            </span>
+            <span
+                className={currentPage === totalPages ? "text-neutral-500" : "cursor-pointer hover:text-neutral-500 text-neutral-300"}
+                onClick={() => toPageHandler(totalPages)}>
+          <FaAngleDoubleRight/>
+      </span>
+        </div>
+    );
 };
 
 export default Paginator;
