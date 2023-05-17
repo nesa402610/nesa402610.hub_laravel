aed0e26ce31f018a8cda5eba9678f0d6c0f7b647
nesa402610 <argyleferris@gmail.com>
2023-05-17 17:11:11 +0300

    Лоадер изменил визуально

--- resources/js/routes/HCollection/HAnime/HAnimePage.tsx
+++ resources/js/routes/HCollection/HAnime/HAnimePage.tsx
@@ -20,25 +20,36 @@ const HAnimePage: FC = () => {
     const {filter} = useAppSelector(state => state.collection)
     const passkey = localStorage.getItem("passkey");
     const [page, setPage] = useState(1);
-    const {data, isLoading, isFetching} = useGetAllAnimeQuery({passkey, page, query: filter});
+    const {data, isLoading, isFetching} = useGetAllAnimeQuery({
+        passkey,
+        page,
+        query: filter
+    });
     const [addTag] = useAddTagToAnimeMutation();
     const [removeTag] = useRemoveTagMutation();
     if (isLoading) return <Loader/>;
 
     return (
-        <div className={"block--dark flex flex-col  gap-4"}>
-            {!data.data.length &&
+        <div className={"block--dark flex flex-col gap-4"}>
+            {!data?.data?.length &&
                 <h2 className={'text-center font-bold text-lg'}>
                     Кажется, по этому запросу еще ничего нет
                 </h2>
             }
-            {!isFetching ? data.data?.map(collectionItem =>
-                <HCollectionCard removeTag={removeTag} addTag={addTag} key={collectionItem.id}
-                                 hover link
-                                 collection={collectionItem}/>
-            ) : <Loader/>}
-            <Paginator currentPage={data.current_page}
-                       totalPages={data.last_page}
+            <div className={'relative flex flex-col gap-4'}>
+                {isFetching &&
+                    <div className={'z-40 absolute rounded-lg bg-neutral-700/70 h-full w-full'}>
+                        <Loader center/>
+                    </div>
+                }
+                {data?.data?.map(collectionItem =>
+                    <HCollectionCard removeTag={removeTag} addTag={addTag} key={collectionItem.id}
+                                     hover link
+                                     collection={collectionItem}/>
+                )}
+            </div>
+            <Paginator currentPage={data?.current_page}
+                       totalPages={data?.last_page}
                        setCurrentPage={setPage}
             />
         </div>
