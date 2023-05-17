2a6e8e0125125e1cd3f175bbb7fead65632fbb6b
nesa402610 <argyleferris@gmail.com>
2023-05-17 17:10:22 +0300

    Правка центрального лоадера

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -16,7 +16,7 @@ class Authenticate extends Middleware
     {
 
         if (!$request->expectsJson()) {
-            return response('', 403, []);
+            return null;
         }
     }
 }
--- resources/js/components/Loader.tsx
+++ resources/js/components/Loader.tsx
@@ -8,7 +8,7 @@ interface LoaderProps {
 
 const Loader: FC<LoaderProps> = ({text = 'Загрузка', center = false}) => {
     return (
-        <div className={`m-4 flex justify-center items-center ${center ? 'justify-center h-full' : ''}`}>
+        <div className={`m-4 flex justify-center items-center ${center ? 'sticky w-full h-screen top-0 left-0' : ''}`}>
             <h2 className={'text-center font-bold text-xl'}>{text}...</h2>
             <RiLoader3Fill size={'2rem'} className={'animate-spin'}/>
         </div>
