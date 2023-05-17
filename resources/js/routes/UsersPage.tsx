637355171477fed0146ec3119023ce056833cdf5
nesa402610 <argyleferris@gmail.com>
2023-05-17 17:10:52 +0300

    Обработка ошибки

--- resources/js/routes/UsersPage.tsx
+++ resources/js/routes/UsersPage.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import {Link} from "react-router-dom";
 import {useGetAllUsersQuery} from "../services/userService";
 import Loader from "../components/Loader";
+import Error from '../components/Error'
 
 const UsersPage = () => {
-    const {data: users, isLoading} = useGetAllUsersQuery()
+    const {data: users, isLoading, isError} = useGetAllUsersQuery()
 
     if (isLoading) return <Loader/>
+    if (isError) return <Error/>
     return (
         <div className={"m-4"}>
             <div className={"flex flex-col gap-4"}>
