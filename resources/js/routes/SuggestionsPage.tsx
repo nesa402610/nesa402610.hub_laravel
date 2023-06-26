faf35580d150288ea24b73b6e6e9f41d97a5e963
nesa402610 <argyleferris@gmail.com>
2023-06-26 14:17:27 +0300

    Фикс вузала

--- resources/js/routes/SuggestionsPage.tsx
+++ resources/js/routes/SuggestionsPage.tsx
@@ -38,27 +38,27 @@ const SuggestionsPage: FC = () => {
         }
         {tasks.map(task =>
           <div key={task.id}
-               className={"block--light flex justify-between relative border-2 " + (task.status === 1 ? "border-green-600 border-2" : task.status === 2 ? "opacity-60 border-red-700 " : task.status === 3 ? "border-amber-700 border-2" : "border-0")}>
-            <div>
+               className={"block--light flex md:flex-row xs:flex-col justify-between relative border-2 " + (task.status === 1 ? "border-green-600 border-2" : task.status === 2 ? "opacity-60 border-red-700 " : task.status === 3 ? "border-amber-700 border-2" : "border-0")}>
               <div>
-                <span className={"italic font-bold"}>{task.title}</span>
+                  <div>
+                      <span className={"italic font-bold"}>{task.title}</span>
+                  </div>
+                  <div className={"overflow-hidden overflow-ellipsis"}>
+                      <span>{task.body}</span>
+                  </div>
               </div>
-              <div className={"overflow-hidden overflow-ellipsis"}>
-                <span>{task.body}</span>
-              </div>
-            </div>
-            <div className={"flex flex-col items-end"}
-                 onMouseEnter={() => adminMenu(task.id)}
+              <div className={"flex flex-col items-end"}
+                   onMouseEnter={() => adminMenu(task.id)}
                  onMouseLeave={() => setHover(0)}
             >
-              <span>{moment(task.created_at).format("DD.MM.YYYY HH:MM:ss")}</span>
-              <div className={"flex gap-4"}>
+                  <span>{moment(task.created_at).format("DD.MM.YYYY HH:MM:ss")}</span>
+                  <div className={"flex gap-4 whitespace-nowrap"}>
                             <span>
                                 {task.status === 1 ? "К реализации" : task.status === 2 ? "Отсеяно" : task.status === 3 ? "Возможно" : "Не рассмотрено"}
                         </span>
-                <Link to={"/profile/" + task.author}>{task.user.name} {task.user.lastName}</Link>
-              </div>
-              <Task__actions hover={hover} task={task}/>
+                      <Link to={"/profile/" + task.author}>{task.user.name} {task.user.lastName}</Link>
+                  </div>
+                  <Task__actions hover={hover} task={task}/>
             </div>
           </div>
         )}
