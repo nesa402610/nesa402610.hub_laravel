d5609df6b5ef5d671a374ca70be0352a5643952f
nesa402610 <argyleferris@gmail.com>
2023-06-29 16:23:39 +0300

    Роли юзеров

--- database/migrations/2023_06_29_112929_create_roles_table.php
+++ database/migrations/2023_06_29_112929_create_roles_table.php
@@ -12,7 +12,8 @@ return new class extends Migration {
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('');
+            $table->string('name');
+            $table->string('custom_name')->nullable();
             $table->timestamps();
         });
     }
