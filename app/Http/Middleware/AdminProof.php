f92a5a1d7c286356bd03e31cb64c8efb6e30dc55
nesa402610 <argyleferris@gmail.com>
2023-06-30 16:31:09 +0300

    Работа через роли

--- app/Http/Middleware/AdminProof.php
+++ app/Http/Middleware/AdminProof.php
@@ -15,7 +15,7 @@ class AdminProof {
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
     public function handle(Request $request, Closure $next) {
-        if (Auth::user()->id !== 1 && Auth::user()->id !== 3) {
+        if (Auth::user()->role === 'Admin') {
             return response(`Ты куда лезешь? Уебывай от сюда. Тебя не записали в админы`, 403);
         }
         return $next($request);
