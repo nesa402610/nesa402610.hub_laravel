9edfe355e722091c12b175be534a96c50b4842a9
nesa402610 <argyleferris@gmail.com>
2023-07-10 18:28:15 +0300

    Изменил верстку

--- resources/js/components/admin/List.tsx
+++ resources/js/components/admin/List.tsx
@@ -10,13 +10,13 @@ interface ListProps {
 const List: FC<ListProps> = ({data, updateFn}) => {
     if (!data) return null;
     return (
-        <>
+        <div className={'flex flex-col gap-4 flex-1'}>
             {
                 data?.map(({name, id, type}) =>
                     <ListItem name={name} id={id} key={id} updateFn={updateFn} tag={type}/>
                 )
             }
-        </>
+        </div>
     );
 };
 
