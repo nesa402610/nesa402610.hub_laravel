9293385c43ffe71c91be6f5736c3734598d95014
nesa402610 <argyleferris@gmail.com>
2023-07-10 18:28:28 +0300

    Теги разделены на Rx и FF

--- resources/js/routes/admin/Tags.tsx
+++ resources/js/routes/admin/Tags.tsx
@@ -2,15 +2,23 @@ import React from "react";
 import {useCreateTagMutation, useGetTagsQuery, useUpdateTagMutation} from "../../services/Collections/TagService";
 import List from "../../components/admin/List";
 import QuickAdder from "../../components/admin/QuickAdder";
+import Loader from "../../components/Loader";
 
 const Tags = () => {
-    const {data} = useGetTagsQuery();
+    const {data, isLoading} = useGetTagsQuery();
     const [createTag, {error}] = useCreateTagMutation();
     const [updateTag] = useUpdateTagMutation();
+    if (isLoading) return <Loader/>
+    const RxData = data.filter(tag => tag.type === 1)
+    const FFData = data.filter(tag => tag.type === 0)
+    console.log(RxData)
     return (
         <div className={"flex flex-col gap-4"}>
             <QuickAdder createFn={createTag} error={error} length={data?.length} tag/>
-            <List data={data} updateFn={updateTag}/>
+            <div className={'flex gap-4 justify-between'}>
+                <List data={RxData} updateFn={updateTag}/>
+                <List data={FFData} updateFn={updateTag}/>
+            </div>
         </div>
     );
 };
