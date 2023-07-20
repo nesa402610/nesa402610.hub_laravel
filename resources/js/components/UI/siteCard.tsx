326b3c49ae08210306deec656dd683948732469c
nesa402610 <argyleferris@gmail.com>
2023-07-20 14:50:19 +0300

    Реформатирование

--- resources/js/components/UI/siteCard.tsx
+++ resources/js/components/UI/siteCard.tsx
@@ -6,24 +6,24 @@ import noImage from "../../img/noimage.png";
 import {IProjectProps} from "../../types/Project";
 
 const SiteCard: FC<IProjectProps> = ({project}) => {
-  const openPageHandler = () => {
-    if (project.previewURL) window.open(project.previewURL, "_blank");
-  };
-  return (
-    <div onClick={openPageHandler}
-         className={"projectCard "
-           + (project.status.toLowerCase() === "planned" ? " opacity-70 cursor-default planned" : ` ${project.status.toLowerCase()}`)
-         }
-    >
-      <ProjectOverview project={project}/>
-      <div className={"h-full"}>
-        {project.image ? <img className={"w-full"} src={project.image} alt=""/> :
-          <div className={"h-full bg-cover bg-center"} style={{backgroundImage: `url(${noImage})`}}/>
-        }
-      </div>
-      <ProjectInfo project={project}/>
-    </div>
-  );
+    const openPageHandler = () => {
+        if (project.previewURL) window.open(project.previewURL, "_blank");
+    };
+    return (
+        <div onClick={openPageHandler}
+             className={"projectCard "
+                 + (project.status.toLowerCase() === "planned" ? " opacity-70 cursor-default planned" : ` ${project.status.toLowerCase()}`)
+             }
+        >
+            <ProjectOverview project={project}/>
+            <div className={"h-full"}>
+                {project.image ? <img className={"w-full"} src={project.image} alt=""/> :
+                    <div className={"h-full bg-cover bg-center"} style={{backgroundImage: `url(${noImage})`}}/>
+                }
+            </div>
+            <ProjectInfo project={project}/>
+        </div>
+    );
 };
 
 export default SiteCard;
