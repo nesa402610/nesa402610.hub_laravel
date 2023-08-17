d792ece369231a7170918d9659145d2c527ec4c1
nesa402610 <argyleferris@gmail.com>
2023-08-18 02:05:29 +0300

    Лишнее убрал

--- resources/js/components/HCollection/CollectionCard/CollectionDescription.tsx
+++ resources/js/components/HCollection/CollectionCard/CollectionDescription.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import {parseBbCode} from '../../../helpers/BBParser';
 
 const CollectionDescription = ({description}) => {
-    const tags: string[] = ['character', 'b', 'i', 'u', 'url', 'img', 'quote', 'spoiler']
-
-
     return (
         <div>
             <h3 className={"mt-4 font-bold"}>Описание</h3>
