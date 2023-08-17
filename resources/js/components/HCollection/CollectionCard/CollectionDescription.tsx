414b3e60d42d200bb8e8720bf13516e2aada42d7
nesa402610 <argyleferris@gmail.com>
2023-08-17 17:49:40 +0300

    Вроде как убираю все теги из описания

--- resources/js/components/HCollection/CollectionCard/CollectionDescription.tsx
+++ resources/js/components/HCollection/CollectionCard/CollectionDescription.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const CollectionDescription = ({description}) => {
+    const tags: string[] = ['character', 'b', 'i', 'u', 'url', 'img', 'quote', 'spoiler']
+    const [newDesc, setNewDesc] = useState<string>(description);
+    useEffect(() => {
+        if (!newDesc) return
+        for (const tag of tags) {
+            const openTag = new RegExp("\\[" + tag + "(.*?)]", "gm");
+            const closeTag = new RegExp("\\[/" + tag + "(.*?)]", "gm");
+            setNewDesc(prev => prev.replace(openTag, '').replace(closeTag, '') ?? 'Забыли описание добавить')
+        }
+    }, []);
+
     return (
         <div>
             <h3 className={"mt-4 font-bold"}>Описание</h3>
-            <p>{description ? description : 'Описание отсутствует...'}</p>
+            <p>{newDesc ?? description}</p>
         </div>
     );
 };
